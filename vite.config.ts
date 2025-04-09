7d9eefaeb0b69671c456910444b54700aa13d1e7
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2025-04-09 07:46:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/test-react-app',
-  publicDir: 'public',
+  base: '/Capability-Portfolio',
   build: {
     outDir: 'dist'
   }
