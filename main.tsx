e40625cd74b2263a2df822a62a08d1ea21070e4d
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2025-04-09 09:07:28 +0200

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Remove Bootstrap import if you're using the CDN
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
