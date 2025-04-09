fb7f7e821beb95b660629b6afb8cba112c0ea6b2
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2025-04-09 07:14:36 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, series, parallel, watch } = require('gulp');
+const { src, dest, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -7,32 +7,16 @@ const terser = require('gulp-terser');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 
-// Paths for source and output files
 const paths = {
-  styles: {
-    src: 'src/scss/**/*.scss',
-    dest: 'dist/css/'
-  },
-  scripts: {
-    src: 'src/js/**/*.js',
-    dest: 'dist/js/'
-  },
-  images: {
-    src: 'src/images/**/*.{jpg,png,gif,svg}',
-    dest: 'dist/images/'
-  },
-  html: {
-    src: 'src/**/*.html',
-    dest: 'dist/'
-  }
+  styles: { src: 'src/scss/**/*.scss', dest: 'public/css/' },
+  scripts: { src: 'src/js/**/*.js', dest: 'public/js/' },
+  images: { src: 'src/images/**/*.{jpg,png,gif,svg}', dest: 'public/images/' }
 };
 
-// Clean the dist folder
 function clean() {
-  return del(['dist']);
+  return del(['public/css', 'public/js', 'public/images']);
 }
 
-// Compile Sass to CSS with autoprefixing and minification
 function styles() {
   return src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
@@ -40,44 +24,17 @@ function styles() {
     .pipe(dest(paths.styles.dest));
 }
 
-// Minify JavaScript
 function scripts() {
   return src(paths.scripts.src)
     .pipe(terser())
     .pipe(dest(paths.scripts.dest));
 }
 
-// Optimize images
 function images() {
   return src(paths.images.src)
     .pipe(imagemin())
     .pipe(dest(paths.images.dest));
 }
 
-// Copy HTML files
-function html() {
-  return src(paths.html.src)
-    .pipe(dest(paths.html.dest));
-}
-
-// Watch for file changes during development
-function watchFiles() {
-  watch(paths.styles.src, styles);
-  watch(paths.scripts.src, scripts);
-  watch(paths.images.src, images);
-  watch(paths.html.src, html);
-}
-
-// Define tasks
-exports.clean = clean;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.images = images;
-exports.html = html;
-exports.watch = watchFiles;
-
-// Build task for production (runs clean, then all tasks in parallel)
 exports.build = series(clean, parallel(styles, scripts, images));
-
-// Default task (same as build)
 exports.default = exports.build;
